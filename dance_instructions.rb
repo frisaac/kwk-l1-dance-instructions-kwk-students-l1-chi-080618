9fe819a1afc40bf034a0534a7394fd2242100445
frisaac <frisaac@stu.naperville203.org>
2018-08-07 20:39:52 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,32 @@
-
 # Code your methods below
+def assume_stance
+  puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def base_footwork
+  puts "1. Lift right foot"
+  puts "2. Return right foot to the ground"
+  puts "3. Finish with a small skip-step backward"
+  skip-steps
+end
+  
+
+  
+def leg_movement
+  puts "Step right
+  Step left
+  Step right
+  Step right
+  Step left
+  Step right
+  Step left
+  Step left"
+end 
+
+assume_stance()
+bust_a_move()
+footwork()
+
+bob_the_reins while leg_movement EIGHTIMES
+
+lasso while leg_movement EIGHTIMES
